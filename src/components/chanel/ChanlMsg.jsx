a9059681121236dd184471dec5c07a3a215ea1f4
Jose David Mustelier Rodriguez <j.davidmr1016@gmail.com>
2023-08-24 23:46:37 -0400

    Finished Channel view with all the users pictures and names randomly

--- src/components/chanel/ChanlMsg.jsx
+++ src/components/chanel/ChanlMsg.jsx
@@ -1,22 +1,22 @@
 import { IMsj } from "../message/messages/IMsj";
 import { UserMsj } from "../message/messages/OtherMsj";
-
+import fakeData from "../../assets/data/users.json";
 export const MessajeList = () => {
   return (
     <div className="flex flex-col h-full w-full">
-      <UserMsj />
+      <UserMsj data={fakeData[9]} />
       <IMsj />
-      <UserMsj />
-      <UserMsj />
-      <UserMsj />
+      <UserMsj data={fakeData[3]} />
+      <UserMsj data={fakeData[5]} />
+      <UserMsj data={fakeData[6]} />
       <IMsj />
       <IMsj />
-      <UserMsj />
-      <UserMsj />
+      <UserMsj data={fakeData[3]} />
+      <UserMsj data={fakeData[8]} />
       <IMsj />
-      <UserMsj />
-      <UserMsj />
-      <UserMsj />
+      <UserMsj data={fakeData[4]} />
+      <UserMsj data={fakeData[2]} />
+      <UserMsj data={fakeData[1]} />
       <IMsj />
       <IMsj />
     </div>
